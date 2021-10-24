2cf8468ed9fa3c336cfb7f79bdcbfb23a375e303
Antonio Noack <antonio-noack@gmx.de>
2021-10-24 13:29:55 +0200

    added F-Wave solver, and added tests for it

---
+++ src/solvers/FWave.h
@@ -0,0 +1,71 @@
+/**
+ * @author Antonio Noack
+ * @section DESCRIPTION
+ * F-Wave solver for the one-dimensional shallow water equations.
+ **/
+#ifndef TSUNAMI_LAB_SOLVERS_FWAVE
+#define TSUNAMI_LAB_SOLVERS_FWAVE
+
+#include "../constants.h"
+
+namespace tsunami_lab {
+  namespace solvers {
+    class FWave;
+  }
+}
+
+class tsunami_lab::solvers::FWave {
+  private:
+    //! gravity, in m/s²
+    static t_real constexpr m_gravity = 9.81; // 3.131557121*3.131557121;
+
+    /**
+     * Inverses a 2x2 matrix.
+     *
+     * @param io_matrix the matrix, which is inversed in-place.
+     * If we need this function more often in the future, it may be better to place it in a linear-algebra-related file.
+     **/
+    static void inverse2x2( t_real io_matrix[2][2] );
+
+    /**
+     * Multiplies a 2x2 matrix with a 2 element vector
+     *
+     * @param i_matrix the matrix.
+     * @param i_vector the input vector.
+     * @param o_vector the output vector.
+     * If we need this function more often in the future, it may be better to place it in a linear-algebra-related file.
+     **/
+    static void transform2x2( t_real i_matrix[2][2],
+                              t_real i_vector[2], 
+                              t_real o_vector[2] );
+
+    /**
+     * Multiplies a 2 element vector with a scalar.
+     * @param i_vector input vector.
+     * @param i_scale scalar.
+     * @param o_vector result vector.
+     **/
+    static void scaleVector2( t_real i_vector[2],
+                              t_real i_scale,
+                              t_real o_vector[2] );
+
+  public:
+    /**
+     * Computes the net-updates.
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_huL momentum of the left side.
+     * @param i_huR momentum of the right side.
+     * @param o_netUpdateL will be set to the net-updates for the left side; 0: height, 1: momentum.
+     * @param o_netUpdateR will be set to the net-updates for the right side; 0: height, 1: momentum.
+     **/
+    static void netUpdates( t_real i_hL,
+                            t_real i_hR,
+                            t_real i_huL,
+                            t_real i_huR,
+                            t_real o_netUpdateL[2],
+                            t_real o_netUpdateR[2] );
+};
+
+#endif // #ifndef TSUNAMI_LAB_SOLVERS_FWAVE
---
+++ src/solvers/FWave.test.cpp
@@ -0,0 +1,225 @@
+/**
+ * @author Antonio Noack
+ * @section DESCRIPTION
+ * Unit tests of the F-Wave solver.
+ **/
+#include <catch2/catch.hpp>
+#define private public
+#include "FWave.h"
+#include "Roe.h"
+#undef public
+
+#define t_real tsunami_lab::t_real
+
+TEST_CASE( "Test the matrix inverse.", "[FWave-Matrix-Inverse]" ) {
+  
+  // input test matrix
+  t_real l_matrix[2][2] = { { 1.0, 2.0 }, { 3.0, 4.0 } };
+  
+  // computed using Wolfram Alpha, inverse([[1,2],[3,4]])
+  t_real l_truth[2][2] = { { -2.0, 1.0 }, { 1.5, -0.5 } };
+  
+  tsunami_lab::solvers::FWave::inverse2x2(l_matrix);
+  
+  for(int j=0;j<2;j++){
+    for(int i=0;i<2;i++){
+      REQUIRE( l_matrix[i][j] == Approx(l_truth[i][j]) );
+    }
+  }
+  
+}
+
+TEST_CASE( "Test vector scale.", "[FWave-Vector-Scale]" ) {
+  
+  // result is obvious enough :D
+  t_real l_vec[2] = { 1.0, 4.0 };
+  t_real l_scale = 3.0;
+  
+  tsunami_lab::solvers::FWave::scaleVector2(l_vec, l_scale, l_vec);
+  
+  REQUIRE( l_vec[0] == Approx(3.0) );
+  REQUIRE( l_vec[1] == Approx(12.0) );
+  
+}
+
+TEST_CASE( "Test matrix * vector.", "[FWave-Matrix*Vector]" ) {
+  
+  // computed using Wolfram Alpha, [[1,2],[3,4]] * [1,4]
+  t_real l_mat[2][2] = { { 1.0, 2.0 }, { 3.0, 4.0 } };
+  t_real l_vec[2] = { 1.0, 4.0 };
+  
+  tsunami_lab::solvers::FWave::transform2x2(l_mat, l_vec, l_vec);
+  
+  REQUIRE( l_vec[0] == Approx(9.0) );
+  REQUIRE( l_vec[1] == Approx(19.0) );
+  
+}
+
+TEST_CASE( "Test when nothing is changing.", "[FWave-netUpdate-Standing]" ) {
+  
+  // result must be ~ 0
+  
+  t_real l_height = 10.0;
+  t_real l_impulse = 4.0;
+  // if our model included friction, the impulse afterwards would be slightly less (transfered into the rock below the sea, and a tiny bit into the air)
+  
+  t_real l_deltaLeft[2];
+  t_real l_deltaRight[2];
+  
+  tsunami_lab::solvers::FWave::netUpdates(l_height, l_height, l_impulse, l_impulse, l_deltaLeft, l_deltaRight);
+  
+  REQUIRE( l_deltaLeft[0] == Approx(0.0) );
+  REQUIRE( l_deltaLeft[1] == Approx(0.0) );
+  REQUIRE( l_deltaRight[0] == Approx(0.0) );
+  REQUIRE( l_deltaRight[1] == Approx(0.0) );
+  
+}
+
+TEST_CASE( "Test breaking dam.", "[FWave-netUpdate-breakingDam]" ) {
+  
+  t_real l_hL = 10.0;
+  t_real l_hR =  0.0;
+  t_real l_uL =  1.0;
+  
+  t_real l_huL = l_hL * l_uL;
+  
+  t_real l_deltaLeft[2];
+  t_real l_deltaRight[2];
+  
+  // manual calculation:
+  // - average height: 5
+  // - sqrt height left: sqrt(10), right: 0
+  // - velocity left: 1, right: 0
+  // - roe velocity = weighted velocity by sqrt height: 1, because only left has fluid
+  // - gravity: 9.81
+  // - gravity term: sqrt(gravity * avg height) ~ 7.003571
+  // - lambdas = wave speeds = roe velocity -/+ gravity term: 1 -/+ 7.003571 = [-6.003571, 8.003571]
+  // - inverse of R matrix (using Wolfram Alpha):
+  //    | 0.571392 -0.0713922 |
+  //    | 0.428608 +0.0713922 |
+  // - delta fields: -10 for height, -10 for impulse
+  // - alphas = R^-1 * [-10, -10]t = [-5, -5]t (Wolfram Alpha)
+  // - net update values:
+  // first alpha and lambda for left, second for right side, because sign(lambda1) != sign(lambda2)
+  // 	alpha * lambda * [1, lambda]
+  // left side: 30.017855, -180.214323760205
+  // right side: -40.017855, -320.2857437602051
+  
+  tsunami_lab::solvers::FWave::netUpdates(l_hL, l_hR, l_huL, 0.0, l_deltaLeft, l_deltaRight);
+  // can be used for comparison, if hR > 0, and when the gravity constants are the same (I use 9.81, the other implementation uses ~9.80665)
+  /*t_real l_deltaLeft2[2];
+  t_real l_deltaRight2[2];
+  tsunami_lab::solvers::Roe::netUpdates(l_hL, l_hR, l_huL, 0.0, l_deltaLeft2, l_deltaRight2);
+  
+  REQUIRE( l_deltaLeft[0]  == Approx(l_deltaLeft2[0]) );
+  REQUIRE( l_deltaLeft[1]  == Approx(l_deltaLeft2[1]) );
+  REQUIRE( l_deltaRight[0] == Approx(l_deltaRight2[0]) );
+  REQUIRE( l_deltaRight[1] == Approx(l_deltaRight2[1]) );*/
+  
+  
+  REQUIRE( l_deltaLeft[0]  == Approx(+30.017855) );
+  REQUIRE( l_deltaLeft[1]  == Approx(-180.21432) );
+  REQUIRE( l_deltaRight[0] == Approx(-40.017855) );
+  REQUIRE( l_deltaRight[1] == Approx(-320.28574) );
+  
+  // switching left and right should produce the same results, except for the signs
+  tsunami_lab::solvers::FWave::netUpdates(l_hR, l_hL, 0.0, l_huL, l_deltaLeft, l_deltaRight);
+  
+  REQUIRE( l_deltaLeft[0]  == Approx(-30.017855) );
+  REQUIRE( l_deltaLeft[1]  == Approx(+180.21432) );
+  REQUIRE( l_deltaRight[0] == Approx(+40.017855) );
+  REQUIRE( l_deltaRight[1] == Approx(+320.28574) );
+  
+}
+
+TEST_CASE( "Test crashing waves.", "[FWave-netUpdate-crashingWaves]" ) {
+  
+  // two waves of equal size and velocity crash together
+  
+  t_real l_hL = 10.0;
+  t_real l_hR = 10.0;
+  t_real l_uL =  1.0;
+  t_real l_uR = -1.0;
+  
+  t_real l_huL = l_hL * l_uL;
+  t_real l_huR = l_hR * l_uR;
+  
+  t_real l_deltaLeft[2];
+  t_real l_deltaRight[2];
+  
+  // manual calculation:
+  // - average height: 10
+  // - sqrt height left: sqrt(10), right: sqrt(10)
+  // - velocity left: 1, right: -1
+  // - roe velocity = weighted velocity by sqrt height: 0, because left & right are equal
+  // - gravity: 9.81
+  // - gravity term: sqrt(gravity * avg height) ~ 9.9045444
+  // - lambdas = wave speeds = roe velocity -/+ gravity term: 0 -/+ 9.9045444 = [-9.9045444, +9.9045444]
+  // - inverse of R matrix (using Wolfram Alpha):
+  //    | 0.5 -0.0504819 |
+  //    | 0.5 +0.0504819 |
+  // - delta fields: 0 for height, -20 for impulse
+  // - alphas = R^-1 * [0, -20]t = [1.00964, -1.00964]t (Wolfram Alpha)
+  // - net update values:
+  // first alpha and lambda for left, second for right side, because sign(lambda1) != sign(lambda2)
+  // 	alpha * lambda * [1, lambda]
+  // left side: -10, +99.045684
+  // right side: -10, -99.045684
+  
+  tsunami_lab::solvers::FWave::netUpdates(l_hL, l_hR, l_huL, l_huR, l_deltaLeft, l_deltaRight);
+  // can be used for comparison when the gravity constants are the same (I use 9.81, the other implementation uses ~9.80665)
+  /*t_real l_deltaLeft2[2];
+  t_real l_deltaRight2[2];
+  tsunami_lab::solvers::Roe::netUpdates(l_hL, l_hR, l_huL, l_huR, l_deltaLeft2, l_deltaRight2);
+  
+  REQUIRE( l_deltaLeft[0]  == Approx(l_deltaLeft2[0]) );
+  REQUIRE( l_deltaLeft[1]  == Approx(l_deltaLeft2[1]) );
+  REQUIRE( l_deltaRight[0] == Approx(l_deltaRight2[0]) );
+  REQUIRE( l_deltaRight[1] == Approx(l_deltaRight2[1]) );*/
+  
+  
+  REQUIRE( l_deltaLeft[0]  == Approx(-10) );
+  REQUIRE( l_deltaLeft[1]  == Approx(+99.045684) );
+  REQUIRE( l_deltaRight[0] == Approx(-10) );
+  REQUIRE( l_deltaRight[1] == Approx(-99.045684) );
+  
+  // switching left and right should produce the same results, except for the signs
+  tsunami_lab::solvers::FWave::netUpdates(l_hR, l_hL, l_huR, l_huL, l_deltaLeft, l_deltaRight);
+  
+  REQUIRE( l_deltaLeft[0]  == Approx(+10) );
+  REQUIRE( l_deltaLeft[1]  == Approx(-99.045684) );
+  REQUIRE( l_deltaRight[0] == Approx(+10) );
+  REQUIRE( l_deltaRight[1] == Approx(+99.045684) );
+  
+}
+
+TEST_CASE( "Test super-sonic.", "[FWave-netUpdate-superSonic]" ) {
+	
+  // damit man Über"schall"-Effekte bekommt, muss |weighted velocity average| > sqrt(gravity * avg height) sein,
+  // also sehr flaches Wasser oder hohe Impulse.
+  
+  t_real l_hL = 10.0;
+  t_real l_hR = l_hL * 0.9; // difference is required, so we can see an effect
+  t_real l_avgHeight = 0.5 * (l_hL + l_hR);
+  t_real l_gravity = tsunami_lab::solvers::FWave::m_gravity;
+  t_real l_requiredVelocity = sqrt(l_gravity * l_avgHeight);
+  t_real l_impulse = - l_hL * l_requiredVelocity * 1.5;// a bit more
+  
+  t_real l_deltaLeft[2];
+  t_real l_deltaRight[2];
+  
+  tsunami_lab::solvers::FWave::netUpdates(l_hL, l_hR, l_impulse, l_impulse, l_deltaLeft, l_deltaRight);
+  
+  t_real l_epsilon = std::numeric_limits<t_real>::epsilon();
+  
+  // wir haben nur eine Änderung im Impuls, aber super-sonic, und nach links, also sollte nur der eine Wert != 0 sein
+  // the actual result of deltaLeft[0] is ~-5e-7 due to rounding errors, and Approx() has no idea of the scale of values, so it reports false for == Approx(0.0)
+  REQUIRE( l_deltaLeft[0] == Approx(0).margin(10 * l_epsilon));
+  REQUIRE( l_deltaLeft[1] > 0.0 );
+  // exactly zero, because nothing is added, because both wave fronts are on the left side
+  REQUIRE( l_deltaRight[0] == Approx(0) );
+  REQUIRE( l_deltaRight[1] == Approx(0) );
+  
+}
+
+#undef t_real
